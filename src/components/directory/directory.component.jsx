71f4d4372a7236c8bcf13f6f6f5c0454c315606e
Rafeh Nawaz <rafeh@Rafehs-MacBook-Pro.local>
2020-01-31 18:56:00 +0500

    updated my menu-items to navigate withRouter Higher Order Components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
@@ -15,6 +15,7 @@ class Directory extends React.Component {
           linkUrl: "shop/hats"
         },
         {
+
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
@@ -47,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
